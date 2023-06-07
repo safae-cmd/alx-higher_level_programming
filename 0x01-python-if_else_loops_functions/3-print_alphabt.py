388f31d2eebdb6c71c94db1801ba803c950e4800
safae-cmd <safaealamimchichi@gmail.com>
2023-06-07 01:19:09 +0000

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for ch_ascii in range(ord('a'), ord('z')+1):
     current = chr(ch_ascii)
-    if current not in 'ej':
+    if current != 'e' and current != 'q':
         print("{}".format(current), end='')
