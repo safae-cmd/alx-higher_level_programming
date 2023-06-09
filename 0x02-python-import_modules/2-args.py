0f0dd9bbbd9c0d1b1fb9e8221ca785e2e472662f
safae-cmd <safaealamimchichi@gmail.com>
2023-06-09 01:41:40 +0000

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,16 +2,11 @@
 if __name__ == "__main__":
     import sys
     args_num = len(sys.argv) - 1
-    if args_num == 0:
-        print("{} arguments.".format(args_num))
-    elif args_num == 1:
+    if args_num == 1:
         print("{} argument:".format(args_num))
     else:
         print("{} arguments:".format(args_num))
     if args_num >= 1:
-        i = 0
-        for arg in sys.argv:
+        for i, arg in enumerate(sys.argv[1:], start=0):
             if args_num != 0:
-                print("{}: {}".format(i, arg))
-            i += 1
-
+                print("{}: {}".format(i + 1, arg))
