4bd3474a11d41586a7fd1811b1ba061298ba3087
safae-cmd <safaealamimchichi@gmail.com>
2023-06-13 01:39:35 +0000

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     l = len(my_list)
-    if idx < 0 or idx >= l:
+    if idx < 0:
+        return (None)
+    if idx >= l:
         return (None)
     return (my_list[idx])
 
