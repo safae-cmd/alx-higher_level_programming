9b9d97c623b44fc4c3aac8b27e15233866fed00e
safae-cmd <safaealamimchichi@gmail.com>
2023-06-15 01:22:00 +0000

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-            new = list(map(lamda a: replace if a == search else a, my_list))
+    new = list(map(lamda a: replace if a == search else a, my_list))
     return new
