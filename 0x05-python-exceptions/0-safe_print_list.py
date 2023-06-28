c0cc3c403efee50c9af11d2e7acf901f6694e0bd
safae-cmd <safaealamimchichi@gmail.com>
2023-06-28 14:13:35 +0000

    task 0 modified

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,5 +10,5 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end='')
         print()
         return x
-    except:
+    except TypeError:
         print("Something is wrong")
