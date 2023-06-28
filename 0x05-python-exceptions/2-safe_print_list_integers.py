440b14927c436684eacce233df3eaf33129e49cc
safae-cmd <safaealamimchichi@gmail.com>
2023-06-28 16:08:35 +0000

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        count = 0
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+                if count == x:
+                    break
+        print()
+        return count
+    except IndexError:
+        raise IndexError("list index is out of range")
