abbe9386879c70acd8aebe3177fbad112741019d
safae-cmd <safaealamimchichi@gmail.com>
2023-06-28 16:43:39 +0000

    task 3 modified

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,11 +2,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        print("Inside result: ", end="")
-        return (result)
     except ZeroDivisionError:
-        print("Inside result: {} ".format(None))
-        return None
+        result = None
     finally:
-        if b != 0:
-            print("{}".format(a / b))
+        print("Inside result: {}".format(result))
+    return result
