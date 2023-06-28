71d293cb3ede3a3d1f2e32d780f1bd05bc1b9456
safae-cmd <safaealamimchichi@gmail.com>
2023-06-28 14:11:09 +0000

    readme file and task 0 uploaded

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for _ in my_list:
+            count += 1
+        if x > count:
+            x = count
+        for i in range(x):
+            print(my_list[i], end='')
+        print()
+        return x
+    except:
+        print("Something is wrong")
