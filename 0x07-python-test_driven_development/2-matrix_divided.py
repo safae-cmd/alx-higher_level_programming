966e10e02cfc58099f5d10ba106f63b70a9aeefe
safae-cmd <safaealamimchichi@gmail.com>
2023-07-05 02:48:43 +0000

    task 1

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    if not all(isinstance(row, list) and all(isinstance(element, (int, float)) for element in row) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    row_size = len(matrix[0])
+    if not all(len(row) == row_size for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    new_matrix = [[round(element / div, 2) for element in row] for row in matrix]
+    return new_matrix
